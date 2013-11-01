0dba42fb0646dbe2d99c9c33d722daf8aa695616
Konrad Gadzina <fenix.b3@gmail.com>
2013-11-01 16:59:34 +0100

    Basic not passing shuffle spec

---
+++ Deckard.Specs/DeckSpecs.cs
@@ -0,0 +1,18 @@
+﻿using Machine.Specifications;
+
+namespace Deckard.Specs
+{
+    [Subject("Deck")]
+    public class when_shuffled
+    {
+        Because of = () =>
+        {
+            deck.Shuffle();
+        };
+
+        It should_be_different_than_before_shuffle = () =>
+        {
+            deck.ShouldNotEqual(deckBeforeShuffle);
+        };
+    }
+}
