ee0f998e5ce911206043e03918d26bef19defe92
Konrad Gadzina <fenix.b3@gmail.com>
2013-11-02 00:58:44 +0100

    Added spec for comparing cards (not yet passing)

---
+++ Deckard.Specs/CardSpecs.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+
+namespace Deckard.Specs
+{
+    [Subject("Card")]
+    public class when_compared_with_another_card
+    {
+        Establish context = () =>
+        {
+            card1 = new Card();
+            card2 = new Card();
+        };
+
+        It should_be_equal_to_card_with_same_attributes = () =>
+        {
+            card1.ShouldEqual(card2);
+        };
+
+        static Card card2;
+        static Card card1;
+    }
+}
