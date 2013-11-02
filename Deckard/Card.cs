62d7f816556733e667d7f9331d3207d0c3ca68dd
Konrad Gadzina <fenix.b3@gmail.com>
2013-11-02 01:03:13 +0100

    Implemented ToString() for Card for more verbose specs' failure messages

--- Deckard/Card.cs
+++ Deckard/Card.cs
@@ -13,5 +13,20 @@ namespace Deckard
         {
             Attributes = new Dictionary<string, string>();
         }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("  Attributes count: {0}\n", Attributes.Count);
+
+            foreach (var attr in Attributes)
+            {
+
+                sb.AppendFormat("  + {0} = {1}\n", attr.Key, attr.Value);
+            }
+
+            return sb.ToString();
+        }
     }
 }
