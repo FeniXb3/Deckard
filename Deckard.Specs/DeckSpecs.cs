1ed088dea80af9679ce527444d76cfe12869beee
Konrad Gadzina <fenix.b3@gmail.com>
2013-11-03 14:33:33 +0100

    Extracted shuffling spec to separated spec file, refactored names in this spec

--- Deckard.Specs/DeckSpecs.cs
+++ Deckard.Specs/DeckSpecs.cs
@@ -4,59 +4,6 @@ using System.Collections.Generic;
 namespace Deckard.Specs
 {
     [Subject("Deck")]
-    public class when_shuffled
-    {
-        Establish context = () =>
-        {
-            shuffler = new RandomNumberSortShuffler();
-            deck = new Deck(shuffler);
-            deckBeforeShuffle = new Deck(shuffler);
-
-            cards = new List<Card>();
-
-            newCard = new Card();
-            newCard.Attributes.Add("suit", "Spades");
-            newCard.Attributes.Add("name", "Ace");
-            cards.Add(newCard);
-            newCard = new Card();
-            newCard.Attributes.Add("suit", "Hearts");
-            newCard.Attributes.Add("name", "Queen");
-            cards.Add(newCard);
-            newCard = new Card();
-            newCard.Attributes.Add("suit", "Clubs");
-            newCard.Attributes.Add("name", "King");
-            cards.Add(newCard);
-            newCard = new Card();
-            newCard.Attributes.Add("suit", "Dimonds");
-            newCard.Attributes.Add("name", "Queen");
-            cards.Add(newCard);
-            newCard = new Card();
-            newCard.Attributes.Add("suit", "Spades");
-            newCard.Attributes.Add("name", "1");
-            cards.Add(newCard);
-
-            deck.Cards = cards;
-            deckBeforeShuffle.Cards = cards;
-        };
-        
-        Because of = () =>
-        {
-            deck.Shuffle();
-        };
-
-        It should_be_different_than_before_shuffle = () =>
-        {
-            deck.ShouldNotEqual(deckBeforeShuffle);
-        };
-
-        static Deck deck;
-        static Deck deckBeforeShuffle;
-        static IShuffler shuffler;
-        static List<Card> cards;
-        static Card newCard;
-    }
-
-    [Subject("Deck")]
     public class when_compared_with_another_deck
     {
         Establish context = () =>
---
+++ Deckard.Specs/ShufflersSpecs.cs
@@ -0,0 +1,58 @@
+﻿using Machine.Specifications;
+using System.Collections.Generic;
+
+namespace Deckard.Specs
+{
+    [Subject("Shuffler")]
+    public class when_used_random_number_sort_shuffler_to_shuffle_deck
+    {
+        Establish context = () =>
+        {
+            shuffler = new RandomNumberSortShuffler();
+            deck = new Deck(shuffler);
+            deckBeforeShuffle = new Deck(shuffler);
+
+            cards = new List<Card>();
+
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Spades");
+            newCard.Attributes.Add("name", "Ace");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Hearts");
+            newCard.Attributes.Add("name", "Queen");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Clubs");
+            newCard.Attributes.Add("name", "King");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Dimonds");
+            newCard.Attributes.Add("name", "Queen");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Spades");
+            newCard.Attributes.Add("name", "1");
+            cards.Add(newCard);
+
+            deck.Cards = cards;
+            deckBeforeShuffle.Cards = cards;
+        };
+
+        Because of = () =>
+        {
+            deck.Shuffle();
+        };
+
+        It should_change_the_card_order_in_deck = () =>
+        {
+            deck.ShouldNotEqual(deckBeforeShuffle);
+        };
+
+        static Deck deck;
+        static Deck deckBeforeShuffle;
+        static IShuffler shuffler;
+        static List<Card> cards;
+        static Card newCard;
+    }
+}
