0443973965f39ba61aa83bd4adddf57210409f5d
Konrad Gadzina <fenix.b3@gmail.com>
2013-11-03 15:40:27 +0100

    Added Knuth Fisher-Yates shuffler

--- Deckard.Specs/ShufflersSpecs.cs
+++ Deckard.Specs/ShufflersSpecs.cs
@@ -55,4 +55,57 @@ namespace Deckard.Specs
         static List<Card> cards;
         static Card newCard;
     }
+
+    [Subject("Shuffler")]
+    public class when_used_Knuth_Fisher_Yates_shuffler_to_shuffle_deck
+    {
+        Establish context = () =>
+        {
+            shuffler = new KnuthFisherYatesShuffler();
+            deck = new Deck(shuffler);
+            deckBeforeShuffle = new Deck(shuffler);
+
+            cards = new List<Card>();
+
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Spades");
+            newCard.Attributes.Add("name", "Ace");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Hearts");
+            newCard.Attributes.Add("name", "Queen");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Clubs");
+            newCard.Attributes.Add("name", "King");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Dimonds");
+            newCard.Attributes.Add("name", "Queen");
+            cards.Add(newCard);
+            newCard = new Card();
+            newCard.Attributes.Add("suit", "Spades");
+            newCard.Attributes.Add("name", "1");
+            cards.Add(newCard);
+
+            deck.Cards = cards;
+            deckBeforeShuffle.Cards = cards;
+        };
+
+        Because of = () =>
+        {
+            deck.Shuffle();
+        };
+
+        It should_change_the_card_order_in_deck = () =>
+        {
+            deck.ShouldNotEqual(deckBeforeShuffle);
+        };
+
+        static Deck deck;
+        static Deck deckBeforeShuffle;
+        static IShuffler shuffler;
+        static List<Card> cards;
+        static Card newCard;
+    }
 }
---
+++ Deckard/KnuthFisherYatesShuffler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deckard.Specs
+{
+    public class KnuthFisherYatesShuffler : IShuffler
+    {
+        static void Swap(IList<Card> list, int indexA, int indexB)
+        {
+            Card tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+
+        public List<Card> Shuffle(Deck deck)
+        {
+            Random R = new Random((int)DateTime.Now.Ticks);
+
+            List<Card> newCardsOrder = deck.Cards.ToList();
+
+            for (int i = 0; i < newCardsOrder.Count; i++)
+            {
+                Swap(newCardsOrder, i, i + R.Next(newCardsOrder.Count - i));
+            }
+
+            return newCardsOrder;
+        }
+    }
+}
